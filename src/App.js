e37c262afce578c584a4f60773b739ebc6f7c9f6
RebeccaNiem <161700704+RebeccaNiem@users.noreply.github.com>
2025-07-21 20:12:44 +0200

    Cleaned starter code and built functions to update input and add tasks
    cleaned up by deleting all unnecessary
     starter code and files generated by create-react-app
    
    function to update input value when user types
    
    Created function to add new task to the list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  function handleInputChange(event) {
+    const newValue = event.target.value;
+    setInput(newValue);
+  }
+
+  function handleAddTask() {
+    setTasks([tasks, input]);
+    setInput("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>To-Do List</h1>
     </div>
   );
 }
